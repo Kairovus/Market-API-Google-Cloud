45d9e118359c64c4c6505d1865f0269aadc5c726
Kairovus <baharirizki3530@gmail.com>
2025-05-14 15:04:46 +0700

    added customer model

---
+++ models/Customer.js
@@ -0,0 +1,59 @@
+
+const pool = require('./db');
+
+exports.getAllCustomers = (callback) => {
+    pool.query('SELECT * FROM Customer', callback);
+};
+
+exports.getCustomerBycustomer_id = (product_id, callback) => {
+    pool.query('SELECT * FROM Customer WHERE Customer = ?', [product_id], callback);
+};
+
+exports.searchCustomerByName = (name, callback) => {
+    pool.query('SELECT * FROM Customer WHERE name LIKE ?', [`%${name}%`], callback);
+};
+
+exports.addProduct = (Customer, callback) => {
+    const {
+      customer_id,
+      name,
+      email,
+      phone,
+      address
+    } = Customer;
+    const query = `INSERT INTO Customer (name, email, phone, address) VALUES (?, ?, ?, ?)`;
+    const values = [
+      name,
+      email,
+      phone,
+      address
+    ];
+    pool.query(query, values, callback);
+  };
+  
+  exports.updateCustomer = (Customer, callback) => {
+    const {
+        customer_id,
+        name,
+        email,
+        phone,
+        address
+    } = Customer;
+    const query = `UPDATE Customer SET name = ?, email = ?, phone = ?, address = ? WHERE customer_id = ?`;
+    const values = [
+        name,
+        email,
+        phone,
+        address
+    ];
+    pool.query(query, values, callback);
+  };
+  
+  exports.deleteProduct = (customer_id, callback) => {
+    pool.query(
+      "DELETE FROM Customer WHERE customer_id = ?",
+      [customer_id],
+      callback
+    );
+  };
+  
--- models/Products.js
+++ models/Products.js
@@ -1,32 +1,74 @@
-
-const pool = require('./db');
+const pool = require("./db");
 
 exports.getAllProducts = (callback) => {
-    pool.query('SELECT * FROM Product', callback);
+  pool.query("SELECT * FROM Product", callback);
 };
 
 exports.getProductByproduct_id = (product_id, callback) => {
-    pool.query('SELECT * FROM Product WHERE product_id = ?', [product_id], callback);
+  pool.query(
+    "SELECT * FROM Product WHERE product_id = ?",
+    [product_id],
+    callback
+  );
 };
 
 exports.searchProductByName = (name, callback) => {
-    pool.query('SELECT * FROM Product WHERE name LIKE ?', [`%${name}%`], callback);
+  pool.query(
+    "SELECT * FROM Product WHERE name LIKE ?",
+    [`%${name}%`],
+    callback
+  );
 };
 
 exports.addProduct = (Product, callback) => {
-    const { product_id, name, description, price, stock_quantity,category_id, supplier_id } = Product;
-    const query = `INSERT INTO Product (name, description, price, stock_quantity, category_id, supplier_id) VALUES (?, ?, ?, ?, ?, ?)`;
-    const values = [product_id, name, description, price, stock_quantity,category_id, supplier_id];
-    pool.query(query, values, callback);
+  const {
+    product_id,
+    name,
+    description,
+    price,
+    stock_quantity,
+    category_id,
+    supplier_id,
+  } = Product;
+  const query = `INSERT INTO Product (name, description, price, stock_quantity, category_id, supplier_id) VALUES (?, ?, ?, ?, ?, ?)`;
+  const values = [
+    name,
+    description,
+    price,
+    stock_quantity,
+    category_id,
+    supplier_id,
+  ];
+  pool.query(query, values, callback);
 };
 
 exports.updateProduct = (Product, callback) => {
-    const { product_id, name, description, price, stock_quantity,category_id, supplier_id } = Product;
-    const query = `UPDATE Product SET name = ?, description = ?, price = ?, stock_quantity = ?, category_id = ?, supplier_id = ? WHERE product_id = ?`;
-    const values = [name, description, price, stock_quantity,category_id, supplier_id, product_id];
-    pool.query(query, values, callback);
+  const {
+    product_id,
+    name,
+    description,
+    price,
+    stock_quantity,
+    category_id,
+    supplier_id,
+  } = Product;
+  const query = `UPDATE Product SET name = ?, description = ?, price = ?, stock_quantity = ?, category_id = ?, supplier_id = ? WHERE product_id = ?`;
+  const values = [
+    name,
+    description,
+    price,
+    stock_quantity,
+    category_id,
+    supplier_id,
+    product_id,
+  ];
+  pool.query(query, values, callback);
 };
 
 exports.deleteProduct = (product_id, callback) => {
-    pool.query('DELETE FROM Product WHERE product_id = ?', [product_id], callback);
+  pool.query(
+    "DELETE FROM Product WHERE product_id = ?",
+    [product_id],
+    callback
+  );
 };
