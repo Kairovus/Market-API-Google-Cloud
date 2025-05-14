7494fc6bb95c0fd6d363ce624ae501a7e7563bc4
Kairovus <baharirizki3530@gmail.com>
2025-05-14 21:54:56 +0700

    Created Category Model

--- models/Category.js
+++ models/Category.js
@@ -9,46 +9,34 @@ exports.searchCategoryByName = (name, callback) => {
     pool.query('SELECT * FROM Category WHERE name LIKE ?', [`%${name}%`], callback);
 };
 
-exports.addProduct = (Customer, callback) => {
+exports.addCategory = (Category, callback) => {
     const {
-      customer_id,
-      name,
-      email,
-      phone,
-      address
-    } = Customer;
-    const query = `INSERT INTO Customer (name, email, phone, address) VALUES (?, ?, ?, ?)`;
+      category_id,
+      category_name,
+    } = Category;
+    const query = `INSERT INTO Category (category_name) VALUES (?)`;
     const values = [
-      name,
-      email,
-      phone,
-      address
+      category_name,
     ];
     pool.query(query, values, callback);
   };
   
-  exports.updateCustomer = (Customer, callback) => {
+  exports.updateCategory = (Category, callback) => {
     const {
-        customer_id,
-        name,
-        email,
-        phone,
-        address
-    } = Customer;
-    const query = `UPDATE Customer SET name = ?, email = ?, phone = ?, address = ? WHERE customer_id = ?`;
+        category_id,
+        category_name,
+    } = Category;
+    const query = `UPDATE Category SET category_name = ? WHERE category_id = ?`;
     const values = [
-        name,
-        email,
-        phone,
-        address
+       category_name
     ];
     pool.query(query, values, callback);
   };
   
-  exports.deleteProduct = (customer_id, callback) => {
+  exports.deleteCategory = (category_id, callback) => {
     pool.query(
-      "DELETE FROM Customer WHERE customer_id = ?",
-      [customer_id],
+      "DELETE FROM Customer WHERE category_id = ?",
+      [category_id],
       callback
     );
   };
