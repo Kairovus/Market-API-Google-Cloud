6c42918202b8d2567932126763437dc4ff1da68c
Kairovus <baharirizki3530@gmail.com>
2025-05-16 13:18:09 +0700

    Created supplier route

---
+++ routes/r_supplier.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const router = express.Router();
+const supplierController = require("../controller/supplierController");
+
+// Get all suppliers
+router.get("/", supplierController.getAllSuppliers);
+
+// Search supplier by name
+router.get("/search/:name", supplierController.searchSupplierByName);
+
+// Get supplier by ID
+router.get("/:id", supplierController.getSupplierById);
+
+// Add new supplier
+router.post("/", supplierController.addSupplier);
+
+// Update supplier
+router.put("/", supplierController.updateSupplier);
+
+// Delete supplier
+router.delete("/:id", supplierController.deleteSupplier);
+
+module.exports = router;
